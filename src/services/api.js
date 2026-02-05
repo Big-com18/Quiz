27726933937a0ea89806309fe62f786b90c3ac15
Billy Andreas <billyandreas441@gmail.com>
2026-02-05 11:17:44 +0700

    Upload codingan pertama

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const fetchQuestions = async () => {
+  // Ambil 10 soal, multiple choice
+  const endpoint = 'https://opentdb.com/api.php?amount=10&type=multiple';
+  const data = await axios.get(endpoint);
+  
+  return data.data.results.map((question) => ({
+    ...question,
+    // Acak posisi jawaban
+    answers: [...question.incorrect_answers, question.correct_answer]
+      .sort(() => Math.random() - 0.5)
+  }));
+};
